a87f6ac3d38d658b280cf732e7b48116a3eab04a
Wyatt J. Miller <wjmiller2016@gmail.com>
2019-12-28 22:42:24 -0500

    added app and tv systems

---
+++ src/app.rs
@@ -0,0 +1,19 @@
+// enum used when a certain TV app is being used
+enum ActiveApp {
+    Netflix,
+    Hulu,
+    AmazonPrime,
+    Pandora,
+    Spotify,
+    Crunchyroll,
+    Funimation,
+    VRV,
+}
+
+// enum used to select what kind of TV you are using
+enum TV {
+    Roku,
+    Android,
+    Samsung,
+    Amazon,
+}
